1c60c320bc01591ea9b5ded8b82c93399a4e71ce
Bulat <galik.bulik@gmail.com>
2024-11-14 11:50:59 +0300

    Добавьте файлы проекта.

---
+++ Multithreading/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class MultiThreadingExample
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Начало работы.");
+
+        // Создание двух задач
+        Task task1 = Task.Run(() => LongOperation("Задача 1"));
+        Task task2 = Task.Run(() => LongOperation("Задача 2"));
+
+        // Ожидание завершения обеих задач
+        Task.WaitAll(task1, task2);
+
+        Console.WriteLine("Завершение работы.");
+    }
+
+    // Функция, имитирующая длительную операцию
+    private static void LongOperation(string taskName)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{taskName}: Итерация {i + 1}");
+            Thread.Sleep(1000); // Задержка в 1 секунду
+        }
+    }
+}
